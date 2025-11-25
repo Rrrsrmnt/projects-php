78b9b6b92eef9c7dd2c85c7dd313e9c2586f3842
Rrrsrmnt <sarmientoram7@gmail.com>
2025-11-25 13:25:12 +0800

    Updated

--- test.php
+++ test.php
@@ -2,11 +2,11 @@
 $A = 10;
 $B = 15;
 
-echo "The sum of " .$A. " and " .$B. " is " .($A + $B). "<br>"; 
-echo "The difference of " .$A. " and " .$B. " is " .($A - $B). "<br>";
-echo "The product of " .$A. " and " .$B. " is " .($A * $B). "<br>";
-echo "the quotient of " .$A. " and " .$B. " is ".($A / $B). "<br>";
-echo "The remainder of " .$A. " and " .$B. " is " .($A % $B). "<br>";
-echo "The result of raising " .$A. " to the " .$B. "th power is ".($A ** $B). "<br>";
+echo "The sum of " . $A . " and " . $B . " is " . ($A + $B) . "<br>"; 
+echo "The difference of " . $A . " and " . $B . " is " . ($A - $B) . "<br>";
+echo "The product of " . $A . " and " . $B . " is " . ($A * $B) . "<br>";
+echo "the quotient of " . $A . " and " . $B . " is ". ($A / $B) . "<br>";
+echo "The remainder of " . $A . " and " . $B . " is " . ($A % $B) . "<br>";
+echo "The result of raising " . $A . " to the " . $B . "th power is ". ($A ** $B) . "<br>";
 
 ?>  
